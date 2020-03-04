999b570d7134167fb4d8fdcc9625e08152dcfb70
Mohamed-Ben-Hamouda <medbenhamouda81@gmail.com>
2020-03-04 15:00:57 +0100

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends Component {
+  state = {
+    name: "faltanne",
+    datvalide: "1120",
+    cardnumber: "",
+  }
+  handChange = (a) => {
+    this.setState({
+      [a.target.name]: a.target.value
+    })
+  }
+
+  // (/(\d{4})/g, '$1 ')
+  render() {
+    let reg = /[0-9]/gi
+    return (
+      <div className="credit-card" >
+        <div className="Card">
+          <div className="company-items"><h2>Company Name</h2></div>
+          <div className="puce-items"><img src="https://s4i6r5r7.stackpathcdn.com/wp/wp-content/themes/simoptions/assets/img/sim-card-bg-min.png" width="100px" />  </div>
+          <div className="numcomp-items"><h5>{this.state.cardnumber.padEnd(16, '*').replace(/(.{4})/g, '$1 ')}</h5 ></div>
+          <div><p className="datevalid-items">{this.state.datvalide.slice(0, 2) + '/' + this.state.datvalide.slice(2, 4)}</p>
+            <p className="logo-items">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/1200px-MasterCard_Logo.svg.png" width="120px" />
+            </p></div>
+          <div className="foulan-items">{(this.state.name).toUpperCase()}</div>
+        </div>
+        <div className="input-items">
+          <input type="text" maxLength="16" placeholder="Card Number" name="cardnumber" onChange={this.handChange} /><br />
+          <input className="secondinput-items" name="name" placeholder="Personne Name" maxLength="20" type="text" onChange={this.handChange} /><br />
+          <input type="text" name="datvalide" placeholder="Date Expire" onChange={this.handChange} />
+        </div >
+      </div >
+    )
+  }
 }
 
-export default App;
+
+export default Card;
